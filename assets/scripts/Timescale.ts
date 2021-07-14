d5cad101e6b5bade709bdbf0ba6fafb4b9d7cb78
clouderow <clouderow@gmail.com>
2021-07-14 18:02:55 +0800

    timescale

---
+++ assets/scripts/Timescale.ts
@@ -0,0 +1,77 @@
+
+import { _decorator, Component, Node, CircleCollider2D, Collider2D, Contact2DType, IPhysics2DContact, PhysicsSystem2D, director, Graphics, GraphicsComponent, RigidBodyComponent, RigidBody2D, Label } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Timescale')
+export class Timescale extends CircleCollider2D {
+    touchingCountMap: Map<Array<number>, number> = new Map;
+    @property(Node)
+    target : Node = null!;
+
+    // @property(Graphics)
+    velocity: Graphics = null!;
+    info: Label = null!;
+
+    is_contact = false;
+    start () {
+        // [3]
+        let collider = this;
+        if (collider) {
+            collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
+            collider.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+        }
+        globalThis.ts = this;
+        this.velocity = this.node.getComponent(Graphics);
+        this.info = this.node.getComponent(Label);
+    }
+
+    onBeginContact (selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        // 只在两个碰撞体开始接触时被调用一次
+        if(otherCollider.node != this.target){
+            return;
+        }
+        const contact_type = [selfCollider.tag,otherCollider.tag];
+        // otherCollider.tag
+        director.set_kSpeed(0.1);
+        console.log('onBeginContact',director.getTotalTime(), contact_type,);
+        this.is_contact=true;
+    }
+    onEndContact (selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        if(otherCollider.node != this.target){
+            return;
+        }
+        // 只在两个碰撞体结束接触时被调用一次
+        const contact_type = [selfCollider.tag,otherCollider.tag];
+        director.set_kSpeed(1);
+        console.log('onEndContact', director.getTotalTime(), contact_type);
+        this.is_contact=false;
+    }
+    update(dt:number){
+        if(this.is_contact==true){
+            const body=this.node.getComponent(RigidBody2D);
+            const target_body=this.target.getComponent(RigidBody2D);
+            let l=body?.linearVelocity.length();
+            this.velocity.clear();
+            this.drawLine(0,0,-l*5,0);
+        }
+    }
+
+    	
+    drawLine(x:number, y:number, rx:number, ry:number) {
+        let graphics = this.velocity;
+        graphics.moveTo(x, y);
+        graphics.lineTo(x - rx, y + ry);
+        graphics.stroke();
+    }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.0/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.0/manual/en/scripting/ccclass.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.0/manual/en/scripting/life-cycle-callbacks.html
+ */
--- assets/scripts/game.ts
+++ assets/scripts/game.ts
@@ -1,13 +1,7 @@
 
-import { _decorator, Component, Node, RigidBody, game, SystemEventType } from 'cc';
+import { _decorator, Component, Node, RigidBody, director, SystemEventType, Director, game } from 'cc';
 import {PhysicsSystem2D, Contact2DType, Collider2D, EPhysics2DDrawFlags} from 'cc';
 const { ccclass, property } = _decorator;
-
-game._kSpeed = 1;
-var _originpostUpdate = PhysicsSystem2D.prototype.postUpdate;
-PhysicsSystem2D.prototype.postUpdate = function (dt) {
-    _originpostUpdate.call(this, dt * game._kSpeed);
-}
 
 @ccclass('Game')
 export class Game extends Component {
@@ -17,8 +11,20 @@ export class Game extends Component {
 
     // [2]
     
-    physics_update: (dt: number)=>void = null;
+    onLoad(){
 
+        director._kSpeed = 1;
+        var _originpostUpdate = PhysicsSystem2D.prototype.postUpdate;
+        var _originfixedTimeStep = PhysicsSystem2D.instance.fixedTimeStep;
+        
+        PhysicsSystem2D.prototype.postUpdate = function (dt) {
+            _originpostUpdate.call(this, dt * director._kSpeed);
+        }
+        director.set_kSpeed = function (ratio: number){
+            director._kSpeed = ratio;
+            PhysicsSystem2D.instance.fixedTimeStep = _originfixedTimeStep*ratio;
+        }
+    }
     start () {
         // [3]
         PhysicsSystem2D.instance.debugDrawFlags = EPhysics2DDrawFlags.Aabb |
@@ -26,30 +32,20 @@ export class Game extends Component {
             EPhysics2DDrawFlags.CenterOfMass |
             EPhysics2DDrawFlags.Joint |
             EPhysics2DDrawFlags.Shape;
-        
-        PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this.onBeinContact, this);
-        PhysicsSystem2D.instance.on(Contact2DType.END_CONTACT, this.onEndContact, this);
+        // PhysicsSystem2D.instance.on(Contact2DType.BEGIN_CONTACT, this.onBeinContact, this);
+        // PhysicsSystem2D.instance.on(Contact2DType.END_CONTACT, this.onEndContact, this);
         globalThis.physics2d = PhysicsSystem2D.instance;
-
-        this.physics_update = PhysicsSystem2D.instance.postUpdate;
-
-
         
+    }
+
+    onRestart(event: TouchEvent){
+        game.restart();
     }
 
     
     addContact (c: Collider2D) {
         let count = this.touchingCountMap.get(c.node) || 0;
         this.touchingCountMap.set(c.node, ++count);
-        // PhysicsSystem2D.instance.enable = false;
-        let update = this.physics_update;
-        if (update){
-            // PhysicsSystem2D.instance.update = function(dt: number){
-            //     update(dt/10);
-            // };
-            console.log(update);
-        }
-        
         console.log('add',c.node);
         
     }
